5b22e86a1224cf7a332678bc1d063f359ead413a
jaejeonglee <https://github.com/jaejeonglee>
2022-03-04 13:54:10 +0900

    소수 구하기

---
+++ 1929_소수 구하기.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('./dev/stdin').toString().trim().split('\n');
+const arr= input[i].split(' ')
+   const M = parseInt(arr[0]); //start
+   const N = parseInt(arr[1]); //end
+
+   for (let i=M; i<=N; i++) {
+      
+   }
