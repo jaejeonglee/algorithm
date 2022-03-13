6a9cb6fc40a6a1e6529a221e77af9a0e133d95c6
jaejeonglee <https://github.com/jaejeonglee>
2022-03-13 22:26:54 +0900

    풀이2

--- 2609_최대공약수,최소공배수.js
+++ 2609_최대공약수,최소공배수.js
@@ -6,7 +6,7 @@ let B = Number(input[1])
 let yaksu = 1;
 let baesu = 1;
 // if (A>B) {
-//     for (let i=B; i>=1; i--) {
+//     for (let i=B; i>1; i--) {
 //         if (B%i===0) {
 //             if (A%i===0) {
 //                 yaksu = i;
@@ -41,16 +41,17 @@ let baesu = 1;
 //     yaksu = A
 //     baesu = A
 // }
-    for(let i=2; i<=Math.min(A, B); i++){
-        if(A % i === 0 && B % i === 0){
-            yaksu = i;
+
+    for(let i=2; i<=Math.min(A, B); i++){ //Math.min으로 둘 중 작은 값을 찾아내어 반복문을 돌린다.
+        if(A % i === 0 && B % i === 0){ //A와 B를 i로 나눈 나머지가 둘 다 0일 때
+            yaksu = i; //최대공약수는 i가 된다.
         }
     }
-    while(true){
-      if((baesu % A == 0) && (baesu % B == 0)){
-        break;
+    while(true){ //조건이 참으로 설정되어 있으니까 임의로 빠져나오기 전까지 계속 반복한다.
+      if((baesu % A == 0) && (baesu % B == 0)){ //최소공배수를 A와 B로 나눈 나머지가 둘 다 0일 때만
+        break; //반복문을 빠져나온다.
       }
-      baesu++;
+      baesu++; //최소공배수를 1씩 증가시킨다.
     }
 
 console.log(yaksu)
