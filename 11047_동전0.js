a2b33c2799dc376bd06e7b9b80111e02464b3e72
jaejeonglee <https://github.com/jaejeonglee>
2022-03-22 15:09:18 +0900

    :sparkles: 9461_파도반 수열

--- 11047_동전0.js
+++ 11047_동전0.js
@@ -1,39 +1,45 @@
 const fs = require('fs');
 const input = fs.readFileSync('./dev/stdin').toString().trim().split('\n');
-// let totalMoney = Number(input[0].split(' ')[1])
+let totalMoney = Number(input[0].split(' ')[1])
+console.log('총액:', totalMoney)
 
-// console.log('총액:', totalMoney)
+let totalCount = 0
 
-// let totalCount = 0
-// while(totalMoney>0) {
-//     for (let i=input.length-1; i>0; i--) {
-//         if(input[i]<totalMoney) {
-//             console.log(input[i])
-//             let count = parseInt(totalMoney/input[i])
-//             console.log(count)
-//             totalMoney -= input[i]*count
-//             console.log('잔액:',totalMoney)
-//             totalCount += count
-//         }
-//     }
-// }
-// console.log(totalCount)
-
-
-let N = Number(input[0].split(' ')[0])
-let K = Number(input[0].split(' ')[1])
-
-let coins = []
-
-  for (let i = 1; i <= N; i++) {
-    coins.push(input[i]);
-  }
-
-let count = 0
-for (let j = coins.length; j >= 0; j--) {
-    if (coins[j] <= K) {
-      count += Math.floor(K / coins[j]);
-      K = K % coins[j];
+while(totalMoney>0) {
+    for (let i=input.length-1; i>0; i--) {
+        if(input[i]<=totalMoney) {
+            console.log('시도해볼 금액:',input[i])
+            let count = parseInt(totalMoney/input[i])
+            console.log('횟수:',count)
+            totalMoney -= input[i]*count
+            console.log('잔액:',totalMoney)
+            totalCount += count
+        }
     }
-  }
-console.log(count)
+}
+console.log(totalCount)
+
+
+// let N = Number(input[0].split(' ')[0])
+// let K = Number(input[0].split(' ')[1])
+
+// let coins = []
+//   for (let i = 1; i <= N; i++) {
+//     coins.push(input[i]);
+//   }
+// console.log('동전 종류:',coins)
+// console.log('총액:', K)
+
+// let totalcount = 0
+
+// for (let j = coins.length; j >= 0; j--) {
+//     if (coins[j] <= K) {
+//     console.log('시도해볼 금액:', coins[j])
+//      let count = Math.floor(K / coins[j]);
+//       console.log('횟수',count)
+//       K = K % coins[j];
+//       console.log('잔액:', K)
+//       totalcount += count
+//     }
+//   }
+// console.log(totalcount)
